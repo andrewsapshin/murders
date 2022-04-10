746d7a330e4cfe0197d11439995a281727c52863
andrewsapshin <Brian.Shin@mattson.com>
2022-04-10 10:14:57 -0700

    Adding all the files at once

---
+++ download-data.R
@@ -0,0 +1,3 @@
+url <- "https://github.com/andrewsapshin/TEST-01/blob/79708621dc78d07c9e88c2b8352d74bff97f6d2d/murders.csv"
+dest_file <- "data/murders.csv"
+download.file(url, destfile = dest_file)
